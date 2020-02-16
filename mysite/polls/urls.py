1ac3c41b85b43ef77534ce3772f2be74a48c4ff7
MekImEne <fi_mekideche@esi.dz>
2020-02-16 16:35:01 +0100

    second commit

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns= {
+urlpatterns= [
     url(r'^$', views.index, name="index"), #we cant add anything to the URL 127.0.0.1/polls
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail , name="detail"), #127.0.0.1/polls/1 (question_id)
+    url(r'^(?P<question_id>[0-9]+)/results$', views.results, name="results"),  # 127.0.0.1/polls/1/results
+    url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name="vote"),  # 127.0.0.1/polls/1/vote
 
-}
+]
