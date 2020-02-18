3afe99e1d90af4f8272357a83bf82203eb6a6df8
MekImEne <fi_mekideche@esi.dz>
2020-02-18 21:45:29 +0100

    first commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from django.template import loader, RequestContext
+
 from .models import Question
-# Create your views here.
 
 def index (request):
-    latest_questions = Question.objects.order_by('-pub_date')[:5]
-    output = ", ".join(q.question_text for q in latest_questions)
+    list_questions = Question.objects.order_by('-pub_date')[:5]
+    #output = ", ".join(q.question_text for q in list_questions)
+    #return HttpResponse(output)
     # first code return HttpResponse('Great job guys ! This is the index page of our polls application')
-    return HttpResponse(output)
+
+    #template = loader.get_template('polls/index.html')
+    context = {'list_questions': list_questions}
+
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("This is the detail view of the question: %s" %question_id)
